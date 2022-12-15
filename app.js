641a0d6094dc85ca50d04061863901aa293bd70b
Bjarne68 <bjan2100@student.miun.se>
2022-12-15 10:55:44 -0300

    Lektion 118

--- app.js
+++ app.js
@@ -576,3 +576,259 @@ const groupedByRatings = books1.reduce((groupedBooks, book) => {
   groupedBooks[key].push(book);
   return groupedBooks;
 }, {});
+
+//The old way of adding a default value:
+function multiply(x, y) {
+  if (typeof y === "undefined") {
+    y = 1;
+  }
+  return x * y;
+}
+
+//A slightly shorter version:
+function multiply3(x, y) {
+  y = typeof y === "undefined" ? 1 : y;
+  return x * y;
+}
+
+// The new super clean way of adding defaults!
+function multiply4(x, y = 1) {
+  return x * y;
+}
+
+// Another example!
+const greet1 = (person, greeting = "hi") => {
+  console.log(`${greeting}, ${person}!`);
+};
+
+// Default value of an array:
+const blah = (x, y = [1, 2, 3]) => {
+  console.log(x, y);
+};
+
+// Multiple default values are possible, but rare
+const greet2 = (person, greeting = "hi", punctuation = "!") => {
+  console.log(`${greeting}, ${person} ${punctuation}`);
+};
+
+function giveMeFour(a, b, c, d) {
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
+  console.log("d", d);
+}
+const colors = ["red", "orange", "yellow", "green"];
+// giveMeFour(...colors);
+// giveMeFour(...'GOAT');
+
+const cephalopods = [
+  "dumbo octopus",
+  "humboldt squid",
+  "flamboyant cuttlefish",
+];
+
+const gastropods = ["giant african snail", "banana slug", "variable neon slug"];
+
+const cnidaria = ["fire coral", "moon jelly"];
+
+const mollusca = [...cephalopods, ...gastropods];
+//["dumbo octopus", "humboldt squid", "flamboyant cuttlefish", "giant african snail", "banana slug", "variable neon slug"]
+
+const inverts = [...cnidaria, ...gastropods, ...cephalopods];
+//["fire coral", "moon jelly", "giant african snail", "banana slug", "variable neon slug", "dumbo octopus", "humboldt squid", "flamboyant cuttlefish"]
+
+const cephCopy = [...cephalopods];
+//["dumbo octopus", "humboldt squid", "flamboyant cuttlefish"]
+
+const feline = {
+  legs: 4,
+  family: "Felidae",
+};
+
+const canine = {
+  family: "Caninae",
+  furry: true,
+  legs: 4,
+};
+
+const dog = {
+  ...canine,
+  isPet: true,
+  adorable: true,
+};
+//{family: "Caninae", furry: true, legs: 4, isPet: true, adorable: true}
+
+const houseCat = {
+  ...feline,
+  isGrumpy: true,
+  personality: "unpredictable",
+};
+//{legs: 4, family: "Felidae", isGrumpy: true, personality: "unpredictable"}
+
+const catDog = {
+  ...canine,
+  ...feline,
+};
+//{family: "Felidae", furry: true, legs: 4}
+
+//Order matters! Legs will be 3 here, because we set it AFTER spreading canine.
+const tripod = {
+  ...canine,
+  legs: 3,
+};
+//{family: "Caninae", furry: true, legs: 3}
+
+const catDogClone = {
+  ...catDog,
+};
+
+const random = [
+  ..."hello",
+  {
+    ...catDog,
+  },
+];
+
+//The arguments object is available in every function you write (except arrow functions)
+//It contains all the arguments passed in.
+function sum1() {
+  //It is NOT an array, we have to turn it into one if we want to use array methods
+  const argsArr = [...arguments];
+  return argsArr.reduce((total, currVal) => {
+    return total + currVal;
+  });
+}
+
+// No arguments object inside of arrow functions :(
+const multiply1 = () => {
+  console.log(arguments);
+};
+
+// OLD WAY!
+// function sum() {
+//   const argsArr = [...arguments]
+//   return argsArr.reduce((total, currVal) => {
+//     return total + currVal
+//   })
+// }
+
+// New way using rest:
+function sum1(...nums) {
+  return nums.reduce((total, currVal) => {
+    return total + currVal;
+  });
+}
+
+//We can have named params and then collect the rest into an array:
+function fullName(first, last, ...titles) {
+  console.log("first", first);
+  console.log("last", last);
+  console.log("titles", titles);
+}
+
+// We can use rest parameters in arrow functions!
+const multiply5 = (...nums) => nums.reduce((total, currVal) => total * currVal);
+
+const raceResults = [
+  "Eliud Kipchoge",
+  "Feyisa Lelisa",
+  "Galen Rupp",
+  "Ghirmay Ghebreslassie",
+  "Alphonce Simbu",
+  "Jared Ward",
+];
+
+// The old way:
+// const gold = raceResults[0]
+// const silver = raceResults[1]
+// const bronze = raceResults[2]
+
+// Using Destructuring:
+const [gold, silver, bronze] = raceResults;
+gold; //'Eliud Kipchoge'
+silver; //'Feyisa Lelisa'
+bronze; //'Galen Rupp'
+
+// const [first, , , fourth] = raceResults;
+// first; //'Eliud Kipchoge'
+// fourth; //'Ghirmay Ghebreslassie'
+
+const [winner, ...others] = raceResults;
+winner; //'Eliud Kipchoge'
+others; //["Feyisa Lelisa", "Galen Rupp", "Ghirmay Ghebreslassie", "Alphonce Simbu", "Jared Ward"]
+
+const runner = {
+  first: "Eliud",
+  last: "Kipchoge",
+  country: "Kenya",
+  title: "Elder of the Order of the Golden Heart of Kenya",
+};
+
+// const {
+//   first,
+//   last,
+//   time
+// } = runner;
+
+const { country: nation, title: honorific } = runner;
+
+const { first, last, ...other } = runner;
+
+const results2 = [
+  {
+    first: "Eliud",
+    last: "Kipchoge",
+    country: "Kenya",
+  },
+  {
+    first: "Feyisa",
+    last: "Lilesa",
+    country: "Ethiopia",
+  },
+  {
+    first: "Galen",
+    last: "Rupp",
+    country: "United States",
+  },
+];
+// NESTED DESTRUCTURING
+const [{ first: goldWinner }, { country }] = results2;
+goldWinner; //"Eliud"
+country; //"Ethiopia"
+
+const runner1 = {
+  first: "Eliud",
+  last: "Kipchoge",
+  country: "Kenya",
+  title: "Elder of the Order of the Golden Heart of Kenya"
+}
+
+// Rather than destructuring INSIDE the function body
+// function print(person) {
+//   const {
+//     first,
+//     last,
+//     title
+//   } = person;
+//   console.log(`${first} ${last}, ${title}`)
+// }
+
+// We can unpack the values we want right in the parameter list:
+function print({
+  first,
+  last,
+  title
+}) {
+  console.log(`${first} ${last}, ${title}`)
+}
+
+const response = [
+  'HTTP/1.1',
+  '200 OK',
+  'application/json',
+]
+
+// Also works with array parameters:
+function parseResponse([protocol, statusCode, contentType]) {
+  console.log(`Status: ${statusCode}`)
+}
